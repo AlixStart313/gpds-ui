23a4ea4688bf29ab969ccccb480ec77d0e0155ba
Lisseth Fuentes <105747977+AlixStart313@users.noreply.github.com>
2024-12-04 19:49:06 -0600

    Add files via upload

---
+++ gpds-ui/src/views/Home.vue
@@ -0,0 +1,14 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>Bienvenido a la página de inicio.Hola jenkins,tas vivo?Holaaa</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
